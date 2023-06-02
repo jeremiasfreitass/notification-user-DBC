1430b0dbaccfb2547009f2532daf83edbecd46b6
jeremiasfreitass <jeremiasjmf@hotmail.com>
2023-06-01 21:43:49 -0300

    first commit

---
+++ src/main/java/com/dbc/notification/notificationuser/NotificationUserApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.notification.notificationuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbc/notification/notificationuser/NotificationUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbc.notification.notificationuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
